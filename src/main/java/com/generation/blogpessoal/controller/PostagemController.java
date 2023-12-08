d7fd50549862138b7733660e67fbf1bd4c41d15f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-08 13:06:46 -0300

    add PostagemController

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,21 @@
+package com.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController//para informar ao spring q esta classe é um controlador da nossa api rest
+@RequestMapping("/postagens") //endereço inicial da controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+
+
+	}
