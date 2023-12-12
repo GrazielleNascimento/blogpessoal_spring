78b31c9624ccc4784eb510b40ef028264dd2beba
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 15:19:39 -0300

    add temaController

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -1,13 +1,29 @@
 package com.generation.blogpessoal.controller;
 
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.blogpessoal.model.Tema;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 @RestController
 @RequestMapping("/temas")
 @CrossOrigin(origins="*", allowedHeaders ="*")
 public class TemaController {
 	
-
+	@Autowired
+	private TemaRepository temaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll(){
+		return ResponseEntity.ok(temaRepository.findAll());
+	}
+	
 }
