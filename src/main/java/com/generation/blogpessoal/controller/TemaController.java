36147a1f6368fcde066d1fc23779cc37df6183d8
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 15:48:39 -0300

    add metodo getById

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,10 @@ public class TemaController {
 		return ResponseEntity.ok(temaRepository.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable Long id){
+		return temaRepository.findById(id)
+		.map(resposta -> ResponseEntity.ok(resposta))
+		.orElse(ResponseEntity.notFound().build());
+	}
 }
