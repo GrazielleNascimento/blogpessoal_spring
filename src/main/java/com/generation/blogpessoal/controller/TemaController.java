61b7e3726e9f7917c71d487c70fc10eb7e900d01
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 17:45:37 -0300

    add metodo put para atualizar temas

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -42,5 +42,12 @@ public class TemaController {
 	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
+	@PutMapping
+    public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
+        return temaRepository.findById(tema.getId())
+            .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+            .body(temaRepository.save(tema)))
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
 }
