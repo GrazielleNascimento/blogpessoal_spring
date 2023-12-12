569b467bce7141b845314958e92ed91b4ec66ca7
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 18:44:08 -0300

    add metodo deletar tema

--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -1,18 +1,22 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Tema;
 import com.generation.blogpessoal.repository.TemaRepository;
@@ -48,6 +52,17 @@ public class TemaController {
             .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
             .body(temaRepository.save(tema)))
             .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<Tema> tema = temaRepository.findById(id);
+        
+        if(tema.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        
+        temaRepository.deleteById(id);              
     }
 
 }
