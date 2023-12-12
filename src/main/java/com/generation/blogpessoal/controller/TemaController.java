a707af47bf18b79b85b138f4cb3b82765abaa67d
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 15:01:49 -0300

    add Controller com anotacoes @RestController, @RequestMapping, @CrossOrigin

---
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin(origins="*", allowedHeaders ="*")
+public class TemaController {
+	
+
+}
