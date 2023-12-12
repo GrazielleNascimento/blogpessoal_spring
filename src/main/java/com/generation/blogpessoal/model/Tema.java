832c7caedccc26b7ccaf00431e25a9704dfcd1fc
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-12-12 14:50:06 -0300

    cria class model Tema para modelagem no bd

---
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -0,0 +1,53 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity// define que sera uma tabela
+@Table(name = "tb_temas")//nomeia a tabela
+public class Tema {
+	
+	@Id// Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO INCREMENT
+	private Long id;
+	
+	@NotNull(message = "O atributo Descrição é obrigatório")
+ 	private String descricao;
+	
+	
+	
+	//implementação dos geters e seters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+ 	
+	
+	
+
+}
