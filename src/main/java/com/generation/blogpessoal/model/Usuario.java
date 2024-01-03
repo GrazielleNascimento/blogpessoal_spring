b5667b78304c5df99fd2ddd3069f17d5f798982a
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-01-03 17:08:22 -0300

    add anotacao schema

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -29,6 +30,7 @@ public class Usuario {
 	@NotBlank(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O Atributo Usuário é Obrigatório!") // example@email.com
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
